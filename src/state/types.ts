112047add3c3861fed647fc6ebd627f2575fb6ac
hachiojidev <hachiojidev@protonmail.com>
2021-03-12 16:22:08 +0900

    style(predictions): Header pane

--- src/state/types.ts
+++ src/state/types.ts
@@ -197,6 +197,7 @@ export interface PredictionsState {
   status: PredictionStatus
   currentEpoch: number
   isLoading: boolean
+  isHistoryPaneOpen: boolean
   rounds: {
     [key: string]: RoundResponse
   }
