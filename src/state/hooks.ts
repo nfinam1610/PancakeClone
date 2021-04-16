ClusterH/pancake-frontend

style: Next cards
- Adjustments

src/state/hooks.ts
@@ -16,7 +16,7 @@ import {
   clear as clearToast,
   setBlock,
 } from './actions'
-import { State, Farm, Pool, Block, ProfileState, TeamsState, AchievementState, PriceState, Round } from './types'
+import { State, Farm, Pool, Block, ProfileState, TeamsState, AchievementState, PriceState } from './types'
 import { fetchProfile } from './profile'
 import { fetchTeam, fetchTeams } from './teams'
 import { fetchAchievements } from './achievements'
@@ -250,4 +250,9 @@ export const useGetRounds = () => {
 
 export const useGetCurrentEpoch = () => {
   return useSelector((state: State) => state.predictions.currentEpoch)
+}
+
+export const useGetLiveRound = () => {
+  const { currentEpoch, rounds } = useSelector((state: State) => state.predictions)
+  return rounds[currentEpoch]
 }

nfinam1610/PancakeClone

src/state/hooks.ts
@@ -19,7 +19,7 @@ import {
   clear as clearToast,
   setBlock,
 } from './actions'
-import { State, Farm, Pool, BlockState, ProfileState, TeamsState, AchievementState, PriceState, Round } from './types'
+import { State, Farm, Pool, BlockState, ProfileState, TeamsState, AchievementState, PriceState } from './types'
 import { fetchProfile } from './profile'
 import { fetchTeam, fetchTeams } from './teams'
 import { fetchAchievements } from './achievements'
@@ -252,4 +252,9 @@ export const useGetRounds = () => {
 
 export const useGetCurrentEpoch = () => {
   return useSelector((state: State) => state.predictions.currentEpoch)
+}
+
+export const useGetLiveRound = () => {
+  const { currentEpoch, rounds } = useSelector((state: State) => state.predictions)
+  return rounds[currentEpoch]
 }